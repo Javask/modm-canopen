837c44335fc00f25fbd25fb35b9fe8bc949828ff
Michael Jossen <michael.jossen@rwth-aachen.de>
2023-01-21 22:21:01 +0100

    Properly implement polarity factor

--- src/cia402/factors.hpp
+++ src/cia402/factors.hpp
@@ -10,6 +10,7 @@ namespace cia402
 // Scaling Factor Formula: Internal = User * (Numerator/Divisor)
 struct ScalingFactor
 {
+	bool polarityInverted{false};
 	uint32_t numerator{1};
 	uint32_t divisor{1};
 
@@ -18,7 +19,15 @@ struct ScalingFactor
 	toInternal(Integer user)
 	{
 		static_assert(std::is_integral<Integer>::value);
-		int32_t result = (int32_t)user * numerator / divisor;
+		typedef typename std::conditional<std::is_signed<Integer>::value, int32_t, uint32_t>::type
+			Internal;
+		Internal result = (Internal)user * numerator / divisor;
+
+		if constexpr (std::is_signed<Integer>::value)
+		{
+			if (polarityInverted) result = -result;
+		}
+
 		if (result > std::numeric_limits<Integer>::max())
 		{
 			return std::numeric_limits<Integer>::max();
@@ -35,7 +44,13 @@ struct ScalingFactor
 	toUser(Integer internal)
 	{
 		static_assert(std::is_integral<Integer>::value);
-		int32_t result = (int32_t)internal * divisor / numerator;
+		typedef typename std::conditional<std::is_signed<Integer>::value, int32_t, uint32_t>::type
+			Internal;
+		Internal result = (Internal)internal * divisor / numerator;
+		if constexpr (std::is_signed<Integer>::value)
+		{
+			if (polarityInverted) result = -result;
+		}
 		if (result > std::numeric_limits<Integer>::max())
 		{
 			return std::numeric_limits<Integer>::max();
@@ -50,8 +65,6 @@ struct ScalingFactor
 
 struct Factors
 {
-	uint8_t polarity = 0;  // 0x607E
-
 	// Position Scaling: Scales: 6062,607A,6067,6068,60F4
 	ScalingFactor position{};  // 0x6093.1, 0x6093.2
 
@@ -60,6 +73,20 @@ struct Factors
 
 	// Acceleration Scaling: Scales: 6083,6085
 	ScalingFactor acceleration{};  // 0x6097.1, 0x6097.2
+
+	// Polarity, modifies all. Represents 0x607E
+	inline void
+	setPolarity(uint8_t polarity)
+	{
+		position.polarityInverted = (bool)(polarity & (1 << 7));
+		velocity.polarityInverted = (bool)(polarity & (1 << 6));
+	}
+	inline uint8_t
+	getPolarity() const
+	{
+		return (position.polarityInverted ? (1 << 7) : 0) |
+			   (velocity.polarityInverted ? (1 << 6) : 0);
+	}
 };
 }  // namespace cia402
 }  // namespace modm_canopen
