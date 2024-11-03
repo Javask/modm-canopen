86741f097d708cc2f5fbdef7b0032523396d7f0e
Michael Jossen <michael.jossen@rwth-aachen.de>
2024-11-04 00:55:59 +0100

    Add more sdo error codes

--- src/sdo_error.cpp
+++ src/sdo_error.cpp
@@ -9,9 +9,23 @@ sdoErrorToString(SdoErrorCode code)
 	switch (code)
 	{
 		case SdoErrorCode::NoError:
-			return "No Error";
+			return "No error";
+		case SdoErrorCode::ToggleBitNotAlternated:
+			return "Toggle bit not alternated";
+		case SdoErrorCode::SDOProtocolTimedOut:
+			return "SDO Protocol timed out";
+		case SdoErrorCode::InvalidCommand:
+			return "Invalid command";
+		case SdoErrorCode::InvalidBlockSize:
+			return "Invalid block size";
+		case SdoErrorCode::InvalidSequenceNumber:
+			return "Invalid sequence number";
+		case SdoErrorCode::CRCError:
+			return "CRC Error";
+		case SdoErrorCode::OutOfMemory:
+			return "Out of memory";
 		case SdoErrorCode::UnsupportedAccess:
-			return "Unsupported Access";
+			return "Unsupported access";
 		case SdoErrorCode::ReadOfWriteOnlyObject:
 			return "Read of write only object";
 		case SdoErrorCode::WriteOfReadOnlyObject:
@@ -22,10 +36,36 @@ sdoErrorToString(SdoErrorCode code)
 			return "PDO mapping error";
 		case SdoErrorCode::MappingsExceedPdoLength:
 			return "Mapping exceeds pdo length";
+		case SdoErrorCode::GeneralParameterIncompatibility:
+			return "General parameter incompatibility";
+		case SdoErrorCode::GeneralInternalIncompatibility:
+			return "General internal incompatibility";
+		case SdoErrorCode::HardwareError:
+			return "Hardware error";
+		case SdoErrorCode::DataTypeDoesNotMatchLengthDoesNotMatch:
+			return "DataType does not match, length does not match";
+		case SdoErrorCode::DataTypeDoesNotMatchLengthTooHigh:
+			return "DataType does not match, length too high";
+		case SdoErrorCode::DataTypeDoesNotMatchLengthTooLow:
+			return "DataType does not match, length too low";
+		case SdoErrorCode::SubIndexDoesNotExist:
+			return "SubIndex does not exist";
 		case SdoErrorCode::InvalidValue:
 			return "Invalid value";
+		case SdoErrorCode::ValueOfParameterTooHigh:
+			return "Value of parameter too high";
+		case SdoErrorCode::ValueOfParameterTooLow:
+			return "Value of parameter too low";
+		case SdoErrorCode::MaximumValueLessThanMinimumValue:
+			return "Maximum value less than minimum value";
+		case SdoErrorCode::ResourceUnavailable:
+			return "Resource unavailable";
 		case SdoErrorCode::GeneralError:
 			return "General error";
+		case SdoErrorCode::DataCannotBeTransferred:
+			return "Data cannot be transferred";
+		case SdoErrorCode::NoDataAvailable:
+			return "No data available";
 		default:
 			return "Not in switch-case";
 	}
--- src/sdo_error.hpp
+++ src/sdo_error.hpp
@@ -9,15 +9,34 @@ namespace modm_canopen
 enum class SdoErrorCode : uint32_t
 {
 	NoError = 0,
+	ToggleBitNotAlternated = 0x0503'0000,
+	SDOProtocolTimedOut = 0x0504'0000,
+	InvalidCommand = 0x0504'0001,
+	InvalidBlockSize = 0x0504'0002,       // Block mode only
+	InvalidSequenceNumber = 0x0504'0003,  // Block mode only
+	CRCError = 0x0504'0004,               // Block mode only
+	OutOfMemory = 0x0504'0005,
 	UnsupportedAccess = 0x0601'0000,
 	ReadOfWriteOnlyObject = 0x0601'0001,
 	WriteOfReadOnlyObject = 0x0601'0002,
 	ObjectDoesNotExist = 0x0602'0000,
 	PdoMappingError = 0x0604'0041,
 	MappingsExceedPdoLength = 0x0604'0042,
-	InvalidValue = 0x0609'0030,
-	GeneralError = 0x0800'0000
-	// TODO: add error codes
+	GeneralParameterIncompatibility = 0x0604'0043,
+	GeneralInternalIncompatibility = 0x0604'0047,
+	HardwareError = 0x0606'0000,
+	DataTypeDoesNotMatchLengthDoesNotMatch = 0x0607'0010,
+	DataTypeDoesNotMatchLengthTooHigh = 0x0607'0012,
+	DataTypeDoesNotMatchLengthTooLow = 0x0607'0013,
+	SubIndexDoesNotExist = 0x0609'0011,
+	InvalidValue = 0x0609'0030,             // Download only
+	ValueOfParameterTooHigh = 0x0609'0031,  // Download only
+	ValueOfParameterTooLow = 0x0609'0032,   // Download only
+	MaximumValueLessThanMinimumValue = 0x0609'0036,
+	ResourceUnavailable = 0x060A'0023,
+	GeneralError = 0x0800'0000,
+	DataCannotBeTransferred = 0x0800'0020,
+	NoDataAvailable = 0x0800'0024,
 };
 
 const char*
